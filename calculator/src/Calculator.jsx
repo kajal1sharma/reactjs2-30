50e1fd04446b7382318fd34ef306b9aee7be3b75
kajal <kajal@gmail.com>
2024-01-08 15:24:28 +0530

    ..

--- calculator/src/Calculator.jsx
+++ calculator/src/Calculator.jsx
@@ -1,53 +1,39 @@
 import React from "react";
-import Result from "./Result";
+
 class Calculator extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.number1 = 0;
-        this.number2 = 0;
-        this.result = 0;
-        this.state={result:0}//as state
+    
+    constructor(){
+        super();
+        this.number1=90;
+        this.number2=80;
     }
-
-    print(){
+    
+    addNumber1(){
 
     }
-    read(){
-
-    }
-    // JSX return 
     render(){
         return <div>
-            <h1>Calculator</h1>
-            <div>
-                <label>Number 1</label>
-                <input onChange={(event)=>{
-                    this.number1= Number(event.target.value);
-                }}/>
-                <br/>
-                <label>Number 2</label>
-                <input onChange={(event)=>{
-                    this.number2= Number(event.target.value);
-                }}/>
-            </div>
-            <div>
-                <button onClick={()=>{
-                
-                    this.result=this.number1+this.number2
-                    this.setState({result:(this.number1+this.number2)})
-                    console.log(this.number1,this.number2,this.result)
-                }}>+</button>
-                <button>-</button>
-                <button>/</button>
-                <button>*</button>
-            </div>
+            <h1>
+                Iam a Calculator
+            </h1>
+            <h2>
+                Enter your Numbers
+            </h2>
 
-            <Result  result={this.state.result}/>
+            <input placeholder="number1" onChange={(event)=>{
+                this.number1=event.target.value
+            }} />
+            <br/>
+            <input placeholder="number2" onChange={(event)=>{
+                this.number2=event.target.value
+            }}/>
+            <br/>
+            <button onClick={()=>{
+                console.log(this.number1)
+                console.log(this.number2)
+            }} >Display On Console</button>
         </div>
     }
 }
 
 export default Calculator;
-
-// Class component
